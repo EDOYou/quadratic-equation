de55c2dae17f2770f40fbb128987fd536bdd5aad
Taghiyev <ktaghiyev01@gmail.com>
2022-11-26 22:56:47 +0100

    quadratic-equation

--- src/main/java/com/epam/rd/autotasks/QuadraticEquation.java
+++ src/main/java/com/epam/rd/autotasks/QuadraticEquation.java
@@ -12,7 +12,16 @@ public class QuadraticEquation {
         double b = scanner.nextDouble();
         double c = scanner.nextDouble();
 
+        double determinant = Math.pow(b,2) - 4.0*a*c; // Simple determinant formula
 
+        if (determinant > 0) { // x1 , x2 --> 2 roots , det>0
+            double x1 = ((-1.0)*b + Math.sqrt(determinant))/(2.0*a);
+            double x2 = ((-1.0)*b - Math.sqrt(determinant))/(2.0*a);
+            System.out.println(x1 + " " + x2);
+        } else if (determinant == 0) {  // only 1 root as det=0
+            double x = ((-1.0)*b)/(2.0*a);
+            System.out.println(x);
+        } else System.out.println("no roots"); // NO ROOTS as det<0
 
     }
 
